bef70347b2cfc9b3d1089eb1f823bbc296f7e5db
unknown <ar73mch1k@gmai.com>
2024-12-06 19:41:57 +0100

    Docs

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@ struct Vec2 {
 	float x, y;
 };
 
+// To make smart component define component wiht memory deallocation logic in destructor, and define appropriate move construtor.
+// SmartComponent will take care of memory managing
 struct TransformComponent {
 	Vec2* position;
 
@@ -28,9 +30,15 @@ struct TransformComponent {
 
 int main() {
 
+	//Creating SmartComponent with TransformComponent as a component to wrap aroud
 	SmartComponent smart_component(TransformComponent(5,6));
 
+	//SmartComponent is movable, but non-copyable.
 	SmartComponent smart_component2(std::move(smart_component));
 
 	std::cout << '\n' << ((TransformComponent*)smart_component2.data)->position->x;
+
+	//	Memory managing properties are achived through storing each individual component destructor as a lambda in a FunctiontWrapper member class (src/FunctionWrapper.h)
+	//	Using of std::function as another way to store lambda is possible, but it has too much overhead for my use case
+	//	Also it uses atleast 8x times more space.
 }
