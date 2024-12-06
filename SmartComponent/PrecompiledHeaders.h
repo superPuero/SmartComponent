ddc845e06e4961d7ea3e00af64193c7bf4e63dca
unknown <ar73mch1k@gmai.com>
2024-12-06 19:45:46 +0100

    PrecompiledHeaders

--- SmartComponent/PrecompiledHeaders.h
+++ SmartComponent/PrecompiledHeaders.h
@@ -1,1 +1,13 @@
 #pragma once
+
+#include <iostream>
+#include <string>
+#include <utility>
+#include <memory>
+#include <algorithm>
+#include <typeindex>
+#include <functional>
+#include <vector>
+#include <array>
+#include <unordered_map>
+#include <unordered_set>
